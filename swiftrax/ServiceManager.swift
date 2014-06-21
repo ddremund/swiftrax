9fd84ce93c83866e051bea44930edf736f8e7e00
Derek Remund <ddremund@gmail.com>
2014-06-21 10:56:18 -0500

    stubbed ServiceManager

---
+++ swiftrax/ServiceManager.swift
@@ -0,0 +1,33 @@
+//
+//  ServiceManager.swift
+//  swiftrax
+//
+//  Created by Derek Remund on 6/21/14.
+//  Copyright (c) 2014 Derek Remund. All rights reserved.
+//
+
+import Foundation
+
+class ServiceManager: NSObject {
+    
+    func create() {
+        
+    }
+    
+    func get() {
+        
+    }
+    
+    func list() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+    
+}
