fd663b59ac6a62d3d3cf7cb732f1aee7607059a0
Derek Remund <ddremund@gmail.com>
2014-06-21 00:15:48 -0500

    removed unused function

--- swiftrax/util.swift
+++ swiftrax/util.swift
@@ -9,18 +9,3 @@
 import Foundation
 
 
-func sendRequestToURL(urlPath: String, #method: String, #data: String, #headers: (String, String)[], #delegate: AnyObject) -> NSURLConnection {
-    let url = NSURL(string: urlPath)
-    var request = NSMutableURLRequest(URL: url)
-    request.HTTPMethod = method
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    for (header, value) in headers
-    {
-        request.setValue(header, forHTTPHeaderField: value)
-    }
-    request.HTTPBody = data.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    print(request)
-    var connection = NSURLConnection(request: request, delegate: delegate, startImmediately: true)
-    return connection
-}
-
